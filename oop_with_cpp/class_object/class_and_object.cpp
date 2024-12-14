676f7ca607d0f9f187f5abd192d2f3aa1708663c
thdihan <tanvirhossain20@iut-dhaka.edu>
2024-12-15 00:48:36 +0600

    Note added on class and object cpp file

--- oop_with_cpp/class_object/class_and_object.cpp
+++ oop_with_cpp/class_object/class_and_object.cpp
@@ -5,7 +5,7 @@ using namespace std;
  * The central idea of OOP is placing functions and data togather into a single entity.
  * Key feature of OOP is Data Hiding: means hiding data from part of the program that do not need to access it.
  */
-// Simple Class and objectu.
+// Simple Class and object.
 class SimpleClass {
 
   private: // access modifier 
@@ -23,11 +23,16 @@ class SimpleClass {
     }
 };
 
+/*
+* Dot operator is also called class member access operator.
+* Some object-oriented languages refer to calls to member functions as "messages".
+*/
+
 int main() {
 
   SimpleClass c1, c2;
   
-  c1.setData(1);
+  c1.setData(1); 
   c2.setData(2);
 
   c1.showData();
